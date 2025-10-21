58f7854f288847056761c8dcee40ec467d286ea8
Charlie Wood <549750@chrome.escg.ac.uk>
2025-10-21 15:54:59 +0100

    ...

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,29 +6,86 @@ using TMPro;
 
 public class Enemy : MonoBehaviour
 {
+    enum States
+    {
+        Patrol,
+        Attack,
+    }
+
+
     public Transform player;
     private NavMeshAgent agent;
-    public GameObject deathText;
+    //public GameObject deathText;
 
+    public Transform[] points;
+    private NavMeshAgent nav;
+    private int destPoint;
+    float sightRange, attackRange;
+    bool playerInSight, playerInAttackRange;
+    public LayerMask playerLayer;
+
+    States state;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+
+        state = States.Patrol;
     }
 
     void Update()
     {
-        agent.destination = player.position;
+        playerInSight = Physics.CheckSphere(transform.position, sightRange, playerLayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, playerLayer);
+
+        Chase();
+        if (state == States.Attack)
+        {
+            //execute attack code
+
+            //agent.destination = player.position;
+
+            Attack();
+        }
+
+        if (state == States.Patrol)
+        {
+            // check for player nearby
+            if (!nav.pathPending && nav.remainingDistance < 0.5f && !playerInSight && !playerInAttackRange)
+            {
+                GoToNextPoint();
+            }
+
+        }
     }
 
     void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Player"))
         {
-            deathText.SetActive(true);
-            Time.timeScale = 0f;
+            state = States.Attack;
         }
     }
 
+    void GoToNextPoint()
+    {
+        if (points.Length == 0)
+        {
+            return;
+        }
+
+        nav.destination = points[destPoint].position;
+        destPoint = (destPoint + 1) % points.Length;
+    }
+
+    void Chase()
+    {
+        nav.SetDestination(player.transform.position);
+    }
+
+    void Attack()
+    {
+
+    }
 
 }
